1af5ea062c18888f31d9a88d868e0e32e1432824
Mustafa Sabe Alarab <mustafa777alarab@gmail.com>
2023-03-30 18:28:14 +0200

    remove unused import

--- app/src/main/java/com/example/music_nr/ui/MainActivity.kt
+++ app/src/main/java/com/example/music_nr/ui/MainActivity.kt
@@ -2,16 +2,7 @@ package com.example.music_nr.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.example.music_nr.R
-import com.example.music_nr.adapter.HomeAdapter
-import com.example.music_nr.adapter.HomeInteraction
-import com.example.music_nr.adapter.HomeItem
-import com.example.music_nr.adapter.HomeItemType
-import com.example.music_nr.data.DataManger
-import com.example.music_nr.data.Music
 import com.example.music_nr.databinding.ActivityMainBinding
-import com.example.music_nr.util.toHomeItem
 
 class MainActivity : AppCompatActivity(){
 
